43d1f7c89294781fcbb7730a74d7cf1b222c758a
Goodness Adewuyi <adewuyigoodness1@gmail.com>
2024-10-12 19:59:39 +0100

    First commit.

---
+++ README.md
@@ -0,0 +1,1 @@
+The README.md goes here.
---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+import companyLogo from './assets/logo0.svg'
+import './App.css'
+// import 
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  const countDown = () => {
+    setCount(count + 1)
+  } 
+
+  return (
+    <>
+      <div className="container">
+        <img src={companyLogo} alt="Company Logo" width="200px" height="auto" />
+        <p>Coming Soon...</p>
+        <p>{count}</p>
+        <button onClick={countDown}>Click Me!</button>
+      </div>
+    </>
+  )
+}
+
+export default App
