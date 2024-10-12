e533dbadb4e41c4a11652683990665fe564d59ee
Goodness Adewuyi <adewuyigoodness1@gmail.com>
2024-10-12 20:40:41 +0100

    Resolved! Browser should display content now.

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+import { BrowserRouter } from 'react-router-dom';
 
-createRoot(document.getElementById('root')).render(
+const rootElement = document.getElementById('root');
+const root = createRoot(rootElement);
+
+const basename = window.location.pathname.split('/')[1]; // Get the current URL path
+
+root.render(
   <StrictMode>
-    <App />
+    <BrowserRouter basename={basename}>
+      <App />
+    </BrowserRouter>
   </StrictMode>,
-)
+);
