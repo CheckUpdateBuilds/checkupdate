64e6591855517a233ec06ef1e02c62407b27bfce
Goodness Adewuyi <adewuyigoodness1@gmail.com>
2024-11-01 15:06:12 +0100

    Completed footer.

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,7 +1,7 @@
 // import React from 'react'
 import "./Footer.css";
 import companyLogo from '../assets/logo2.svg'
-// import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 const Footer = () => {
   return (
@@ -9,76 +9,116 @@ const Footer = () => {
     
         <div className="footer">
           <div className="container-fluid">
-          <a className="navbar-brand logo-move" href="#"><img src={companyLogo} alt="CheckUpdate Logo" style={{ width: '150px' }} /></a>
+          <a className="navbar-brand logo-move" href="#"><img src={companyLogo} alt="CheckUpdate Logo" style={{ width: '100px' }} /></a>
           </div>
 
           <div className="navback">
             <a href="#">Back to top <i className="fa fa-arrow-up" aria-hidden="true"></i></a>
           </div>
 
-          {/* <div className="details">
-            <div className="company">
-              <p className="pcompany">Our Company</p>
-              <span className="">About Us</span>
-              <span className="pc02">Privacy Policy</span>
+          <section>
+            <div className="first">
+              <p className="pT pt1">Our Company</p>
 
-              <p  className="pt-1">
-                <span className="pc01">Help & Support</span>
-                <span className="pc02">Terms of service</span>
-              </p>
+              <div className="com1">
+                <span className=""><a href="#">About Us</a></span>
+                <span className="hs"><a href="#">Privacy Policy</a></span>
+              </div>
+              
+              <div className="com2">
+                <span className=""><a href="#">Help & Support</a></span>
+                <span className="hs"><a href="#">Terms of Service</a></span>
+              </div>
 
-              <p className="pc03">
-                <span>Contact Us</span>
-                <span className="pc02">Vacancy</span>
-              </p>
+              <div className="com3">
+                <span className=""><a href="#">Contact Us</a></span> <span className="hs"><a href="#">Vacancy</a></span>
+              </div>
+            
             </div>
 
+            <div className="second">
+              <div className="pT">Category</div>
 
-            <div className="category">
-              <p className="pcompany">Category</p>
-              <p>
-                <span><Link to={'/'} style={{textDecoration:'none', color:'white'}}>News</Link></span>
+              <div className="cat1">
+                <span className="">
+                  <Link to={'/'} style={{textDecoration:'none', color:'white'}}>News</Link>
+                </span>
+                <span className="edu">
+                  <Link to={'/education'} style={{textDecoration:'none', color:'white'}}>Education</Link>
+                </span>
+                <span className="edu">
+                  <Link to={'/bubsiness'} style={{textDecoration:'none', color:'white'}}>Business</Link>
+                </span>
+                <span className="edu">
+                  <Link to={'/earth'} style={{textDecoration:'none', color:'white'}}>Earth & Wildlife</Link>
+                </span>
+              </div>
 
-                <span><Link to={'/education'} style={{textDecoration:'none', color:'white'}}>Education</Link></span>
-
-                <span><Link to={'/business'} style={{textDecoration:'none', color:'white'}}>Business</Link></span>
-
-                <span><Link to={'/earth'} style={{textDecoration:'none', color:'white'}}>Earth & Wildlife</Link></span>
-              </p>
-              <p className="d-flex mx-5">
-                <span className="nav-link"><Link to={'/sports'} style={{textDecoration:'none', color:'white'}}>Sports</Link></span>
-
-                <span className="nav-link"><Link to={'/lifestyle'} style={{textDecoration:'none', color:'white'}}>Lifestyle</Link></span>
-
-                <span className="nav-link"><Link to={'/science'} style={{textDecoration:'none', color:'white'}}>Science & Tech</Link></span>
-
-                <span className="nav-link"><Link to={'/opportunities'} style={{textDecoration:'none', color:'white'}}>Opportunities</Link></span>
-              </p>
-
-              <p className="d-flex">
-                <span className="nav-link"><Link to={'/entertainment'} style={{textDecoration:'none', color:'white'}}>Entertainment</Link></span>
-
-                <span className="nav-link"><Link to={'/politics'} style={{textDecoration:'none', color:'white'}}>Policitcs</Link></span>
-
-                <span className="nav-link"><Link to={'/culture'} style={{textDecoration:'none', color:'white'}}>Culture & Religion</Link></span>
-
-                <span className="nav-link"><Link to={'/awards'} style={{textDecoration:'none', color:'white'}}>CheckUpdate Awards</Link></span>
-              </p>
-            </div>
-
-
-            <div className="socials">
-              <p className="pcompany">Our Socials</p>
-              <span>Connect with us on our social media platforms to enhance your experience</span>
-              <div className="socials-logo">
-                <i className="fas fa-twitter"></i>
-                <i className="fa fa-instagram" aria-hidden="true"></i>
-                <i className="fa fa-facebook" aria-hidden="true"></i>
-                <i className="fa fa-linkedin" aria-hidden="true"></i>
-                <i className="fa fa-youtube" aria-hidden="true"></i>
+              <div className="cat2">
+                <span className="">
+                  <Link to={'/sports'} style={{textDecoration:'none', color:'white'}}>Sports</Link>
+                </span>
+                <span className="edu">
+                  <Link to={'/lifestyle'} style={{textDecoration:'none', color:'white'}}>Lifestyle</Link>
+                </span>
+                <span className="edu">
+                  <Link to={'/science'} style={{textDecoration:'none', color:'white'}}>Science & Tech</Link>
+                </span>
+                <span className="edu">
+                  <Link to={'/opportunities'} style={{textDecoration:'none', color:'white'}}>Opportunities</Link>
+                </span>
+              </div>
+              
+              <div className="cat3">
+                <span>
+                  <Link to={'/entertainment'} style={{textDecoration:'none', color:'white'}}>Entertainment</Link>
+                </span>
+                <span className="edu">
+                  <Link to={'/politics'} style={{textDecoration:'none', color:'white'}}>Politics</Link>
+                </span>
+                <span className="edu">
+                  <Link to={'/culture'} style={{textDecoration:'none', color:'white'}}>Culture & Religion</Link>
+                </span>
+                <span className="edu">
+                  <Link to={'/awards'} style={{textDecoration:'none', color:'white'}}>CheckUpdate Awards</Link>
+                </span>
               </div>
             </div>
-          </div> */}
+
+            <div className="third">
+              <div className="pT">Our Socials</div>
+              <div className="socialWords">Connect with us on our social media platforms to enhance your performance.</div>
+              <div className="social-logos d-flex">
+                <div>
+                  <a href="#">
+                    <i className="fa fa-twitter" aria-hidden="true"></i>
+                  </a>
+                </div>
+                <div>
+                  <a href="#">
+                    <i className="fa fa-instagram" aria-hidden="true"></i>
+                  </a>
+                </div>
+                <div>
+                  <a href="#">
+                    <i className="fa fa-facebook" aria-hidden="true"></i>
+                  </a>
+                </div>
+                <div>
+                  <a href="#">
+                    <i className="fa fa-linkedin" aria-hidden="true"></i>
+                  </a>
+                </div>
+                <div>
+                  <a href="#">
+                    <i className="fa fa-youtube" aria-hidden="true"></i>
+                  </a>
+                </div>
+              </div>
+            </div>
+          </section>
+
+          
         </div>
     
     
--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -3,10 +3,10 @@ import { Link } from 'react-router-dom'
 
 const NotFound = () => {
   return (
-    <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', minHeight: '50vh'}}>
-      <h1>Page Not Found!</h1>
+    <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', minHeight: '100vh'}}>
+      <h1 className='text-center'>Page Not Found!</h1>
       <Link to={'/'} style={{textDecoration: 'none', color: 'black' }}>
-        <button className='btn btn-outline-danger'>Go back to Home</button>
+        <button className='btn m-5'>Go back Home</button>
       </Link>
     </div>
   )
