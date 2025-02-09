736dec0fcc53d2bdffc1fa7c5194b9195d7e79bc
Mofisam <malaosamuel2020@gmail.com>
2025-02-09 14:51:35 +0100

    Editor folder added with some admin stuff

--- src/pages/Editor/Create Article.jsx
+++ src/pages/Editor/Create Article.jsx
@@ -11,47 +11,8 @@ const CreateArticle = () => {
   }, []);
 
   return (
-    <div className="d-flex">
-      {/* Sidebar */}
-      <div className="sidebar bg-danger text-white vh-100 p-4">
-        <ul className="nav flex-column">
-          <li className="nav-item active">
-            <a href="#" className="text-white d-flex align-items-center">
-              <i className="fas fa-tachometer-alt me-2"></i> Dashboard
-            </a>
-          </li>
-          <li className="nav-item">
-            <a href="#" className="text-white d-flex align-items-center">
-              <i className="fas fa-newspaper me-2"></i> My Articles
-            </a>
-          </li>
-          <li className="nav-item">
-            <a href="#" className="text-white d-flex align-items-center">
-              <i className="fas fa-edit me-2"></i> Create Article
-            </a>
-          </li>
-          <li className="nav-item">
-            <a href="#" className="text-white d-flex align-items-center">
-              <i className="fas fa-chart-line me-2"></i> Article Performance
-            </a>
-          </li>
-        </ul>
-      </div>
-
-      {/* Main Content */}
-      <div className="content p-4 flex-grow-1">
-        <div className="header bg-white p-3 shadow-sm d-flex justify-content-between align-items-center">
-          <h1 className="text-danger">Create New Article</h1>
-          <span className="notifications" data-bs-toggle="dropdown">
-            &#128276;
-          </span>
-          <div className="dropdown-menu dropdown-menu-end">
-            <a className="dropdown-item" href="#">Article Approved</a>
-            <a className="dropdown-item" href="#">Article Rejected</a>
-            <a className="dropdown-item" href="#">Feedback on Article</a>
-          </div>
-        </div>
-
+    <div className="">
+        
         {/* Create Article Form */}
         <form>
           <div className="form-group mt-4">
@@ -90,7 +51,6 @@ const CreateArticle = () => {
             <button type="button" className="btn btn-secondary">Save as Draft</button>
           </div>
         </form>
-      </div>
     </div>
   );
 };
