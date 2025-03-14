f6307327e8d8d9fab4e0b0039a18a0774f9d751f
Goodness Adewuyi <adewuyigoodness1@gmail.com>
2025-03-14 14:29:00 +0100

    AI Chat bubble rebuilt.

--- src/Components/Chatbot.jsx
+++ src/Components/Chatbot.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import { Link } from "react-router-dom";
+import { IoChatbubbleOutline } from "react-icons/io5";
 
 const Chatbot = () => {
   return (
     <div>
-      <button className="fixed bottom-10 right-10 bg-companyRed text-white p-4 rounded-full shadow-lg hover:bg-red-800 transition duration-300 z-50 ">
-        <Link to={"/chat"}>Have a conversation with Gemini</Link>
+      <button className="fixed bottom-10 right-10 bg-companyRed text-white p-4 rounded-3xl w-20 shadow-lg hover:bg-red-800 transition duration-300 z-50 ">
+        <Link to={"/chat"} className="flex justify-center items-center">
+          <IoChatbubbleOutline />
+        </Link>
       </button>
     </div>
   );
